ebefb4568552cf94b6dc6ce0bbef00e4de24da46
Marc-Braun-72 <marc.braun72@gmx.de>
2024-07-29 13:20:37 +0200

    html boilerplate

---
+++ up.sh
@@ -0,0 +1,19 @@
+
+git pull
+git add .
+git commit -m "%"
+git push
+
+
+
+windows:  up.bat
+git commit -m "$*"
+
+
+git clone <ssh link von github>
+
+in VS Code öffnen: In der Konsole zum Order (cd ..), dann code <ordnername>
+
+Mac:
+yaml:
+cat ~/.ssh/id_rsa.pub
